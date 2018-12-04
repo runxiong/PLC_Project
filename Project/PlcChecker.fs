a8546c3614423f55cf4d9bde47c98c08c8f386d2
Derek Choi <derek-choi@uiowa.edu>
2018-12-03 23:47:43 -0600

    Got most of the basic types working

--- Project/PlcChecker.fs
+++ Project/PlcChecker.fs
@@ -9,3 +9,90 @@ open Environ
 
 let rec teval (e : expr) (env : plcType env) : plcType =
     // to be implemented 
+    match e with // temp
+    | ConI i -> IntT 
+
+    | ConB b -> BooT
+  
+    | Var x  -> lookup env x 
+
+    | EList l -> l
+  
+    | Prim1 (op, e1) -> 
+      let t1 = teval e1 env in
+      match (op, t1) with
+      | ("-", IntT) -> IntT
+      | ("!", BooT) -> BooT
+      | ("hd", LisT a) -> a
+      | ("tl", LisT a) -> a
+      | ("print", _) -> TupT []
+      | ("ise", LisT _) -> BooT
+      | _   -> failwith "Checker: unknown op, or type error"
+
+    | Prim2 (op, e1, e2) -> 
+      let t1 = teval e1 env in
+      let t2 = teval e2 env in
+      match (op, t1, t2) with
+      | ("*", IntT, IntT) -> IntT
+      | ("/", IntT, IntT) -> IntT
+      | ("+", IntT, IntT) -> IntT
+      | ("-", IntT, IntT) -> IntT
+      | ("<", IntT, IntT) -> BooT
+      | ("<=", IntT, IntT) -> BooT   
+      | ("&&", BooT, BooT) -> BooT
+      | ("=", BooT, BooT) -> BooT   // not sure if this actually covers all cases; should = and != apply between nonequivalent types?    
+      | ("=", IntT, IntT) -> BooT                      
+      | ("=", TupT [], TupT []) -> BooT
+      | ("!=", BooT, BooT) -> BooT                      
+      | ("!=", IntT, IntT) -> BooT                      
+      | ("!=", TupT [], TupT []) -> BooT
+      | ("::", t1, LisT t2) when t1 = t2 -> LisT t1
+      | (";", _, _) -> t2
+      | _   -> failwith "Checker: unknown op, or type error"
+
+    | Let (x, e1, e2) -> 
+      let t = teval e1 env in
+      let env' = (x, t) :: env in
+      teval e2 env'
+  
+    | If (e1, e2, e3) -> 
+      match teval e1 env with
+      | BooT -> let t2 = teval e2 env in
+                 let t3 = teval e3 env in
+                 if t2 = t3 then 
+                   t2
+                 else 
+                   failwith "Checker: 'if' branch types differ"
+
+      | _    -> failwith "Checker: 'if':' condition not Boolean"
+ 
+    (*| Letfun (f, x, xt, e1, rt, e2) -> 
+      let ft = FunT (xt, rt) in
+      let fenv = (x, xt) :: (f, ft) :: env in
+      let lenv = (f, ft) :: env in
+      if teval e1 fenv = rt then 
+        teval e2 lenv
+      else
+        failwith ("Checker: wrong return type in function " + f)*)
+ 
+    | Call (Var f, e) -> 
+      match lookup env f with
+      | FunT (xt, rt) ->
+        if teval e env = xt then 
+          rt
+        else
+          failwith "Checker: type mismatch in function call"
+      | _ -> failwith ("Checker: function " + f + "is undefined")
+ 
+    | Call _ -> failwith "Call: illegal function in call"
+
+    | Tuple es -> TupT (List.map (fun e -> teval e env) es)
+
+    | Sel (e1, n) -> // copied from hw6, need test
+      match teval e1 env with 
+      | TupT ts -> 
+        if 0 < n && n <= List.length ts then
+          List.item (n - 1) ts
+        else
+          failwith "Checker: Tuple index out of range"
+      | _ -> failwith ("Checker: selection operator #" + string n + "applied to non-tuple")
--- Project/PlcInterp.fs
+++ Project/PlcInterp.fs
@@ -6,4 +6,78 @@ open Environ
 
 let rec eval (e : expr) (env : plcVal env) : plcVal =
     // to be implemented
+    match e with // all temp
+    | ConI i -> IntV i
+    | ConB b -> BooV b
+
+    | Var x  ->
+      let v = lookup env x in
+      match v with
+      | IntV  _ -> v
+      | BooV _ -> v 
+      | TupV  _ -> v 
+      | _      -> failwith ("Value of variable _" + x + "_ is not first-order.")
+
+    | EList _ -> TupV []
+    
+    | Prim1 (op, e1) -> 
+      let v1 = eval e1 env in
+      match (op, v1) with
+      | ("-", IntV i) -> IntV (- i)
+      | ("!", BooV b) -> BooV (not b)
+      | ("hd", LisV a) -> a.Head
+      | ("tl", LisV a) -> (List.rev a).Head
+      | ("print", _) -> printfn "%s" (val2string v1); TupV []
+      | ("ise", LisV a) -> BooV (a.IsEmpty)
+      | _   -> failwith "Impossible"
+
+    | Prim2 (op, e1, e2) -> 
+      let v1 = eval e1 env in
+      let v2 = eval e2 env in
+      match (op, v1, v2) with
+      | ("=", _, _) -> BooV (v1 = v2)
+      | ("!=", _, _) -> BooV (not (v1 = v2))
+      | ("<", IntV i1, IntV i2) -> BooV (i1 < i2)
+      | ("<=", IntV i1, IntV i2) -> BooV (i1 <= i2)
+      | ("*", IntV i1, IntV i2) -> IntV (i1 * i2)
+      | ("/", IntV i1, IntV i2) -> IntV (i1 / i2)
+      | ("+", IntV i1, IntV i2) -> IntV (i1 + i2)
+      | ("-", IntV i1, IntV i2) -> IntV (i1 - i2)
+      | ("&&", BooV i1, BooV i2) -> BooV (i1 && i2)
+      | ("::", t1, LisV t2) -> LisV (t1 :: t2)
+      | (";", _, _) -> v2
+      | _   -> failwith "Impossible"
+    
+    | Let (x, e1, e2) -> 
+      let v = eval e1 env in
+      let env2 = (x, v) :: env in
+      eval e2 env2
+    
+    | If (e1, e2, e3) -> 
+      let v1 = eval e1 env in
+      match v1 with
+      | BooV true  -> eval e2 env
+      | BooV false -> eval e3 env
+      | _ -> failwith "Impossible"
+
+    (*| Letfun (f, x, _, e1, _, e2) -> 
+      let env2 = (f, Closure(f, x, e1, env)) :: env in
+      eval e2 env2*)
+
+    (*| Call (Var f, e) -> 
+      let c = lookup env f in
+      match c with
+      | Closure (f, x, e1, fenv) ->
+        let v = eval e env in
+        let env1 = (x, v) :: (f, c) :: fenv in
+        eval e1 env1
+      | _ -> failwith "eval Call: not a function"
+    | Call _ -> failwith "eval Call: not first-order function"*)
+
+    | Tuple es -> TupV (List.map (fun e -> eval e env) es)
+
+    | Sel (e1, n) -> 
+      match eval e1 env with 
+      | TupV vs -> List.item (n - 1) vs
+      | _ -> failwith "Impossible"
 
